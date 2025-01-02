652534706e24656c507650a40bbab7a56a574ac8
Sujit Ale <sujitale01@gmail.com>
2025-01-03 00:25:13 +0545

    d

--- src/app/(protected)/dashboard/[slug]/_components/metrics/chart.tsx
+++ src/app/(protected)/dashboard/[slug]/_components/metrics/chart.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 const MetricsCard = (props: Props) => {
   const { data } = useQueryAutomations()
-  const comments = data?.data.reduce((current, next) => {
+  const comments: number | undefined = data?.data.reduce((current, next) => {
     return current + next.listener?.commentCount!
   }, 0)
 
